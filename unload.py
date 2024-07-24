28334ca18bad9a2b5218b518d9a36b4f8152c87f
Vseva <103783951+Grindelwaldoff@users.noreply.github.com>
2024-07-24 21:28:43 +0300

    saved json to file

--- unload.py
+++ unload.py
@@ -6,8 +6,9 @@ from db.models import AddressModel
 
 async def data_unload():
     # выгрузка данных в json
+    addresses = AddressModel.unload()
     with open('./unload.json', 'w') as file:
-        return json.dump([*AddressModel.unload()], file, indent=4)
+        return json.dump([*addresses], file, indent=4)
 
 
 if __name__ == '__main__':
